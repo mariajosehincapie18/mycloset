2a1ef3e56b0a87802cfbe8da878327a6830238ac
mariajosehincapie18 <mjhincapie18@gmail.com>
2024-10-30 10:10:41 -0500

    comit 1

--- sql/nombretabla1.sql
+++ sql/nombretabla1.sql
@@ -1,3 +1,9 @@
 """
 executes para crear la tabla
 ""
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- src/Controller/Controlador_general.py
+++ src/Controller/Controlador_general.py
@@ -13,7 +13,49 @@ def ObtenerCursor():
     HOST = SecretConfig.PGHOST
     PORT = SecretConfig.PGPORT
     connection = psycopg2.connect(database=DATABASE, user=USER, password=PASSWORD, host=HOST, port=PORT)
-    return connection
+
+    return connection.cursor(), connection
 
 
-cursor = ObtenerCursor()
+
+
+def crear_tablas():
+     
+     return """
+    CREATE TABLE  IF NOT EXISTS usuarios(
+        id SERIAL PRIMARY KEY,
+        nombre VARCHAR(100),
+        contraseña VARCHAR(100),
+        preferenica_estilo VARCHAR(100),
+        colores preferidos VARCHAR(100),
+        ropa favorita VARCHAR(100)
+        );
+    CREATE TABLE IF NOT EXISTS prendas (
+        id_prenda SERIAL PRIMARY KEY,
+        usuario_id INTEGER NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
+        nombre_prenda VARCHAR(100),
+        ocasion VARCHAR(50),
+        talla VARCHAR(10),
+        temporada VARCHAR(50)
+    );
+    CREATE TABLE IF NOT EXISTS prenda_outfit(
+        id_prenda_outfit SERIAL PRIMARY KEY,
+        prenda_id INTEGER NOT NULL REFERENCES prendas(id_prenda) ON DELETE CASCADE,
+        usuario_id INTEGER NOT NULL REFERENCES usuarios(id) ON DELETE CASCADE,
+
+
+     );
+"""
+cursor, connection = ObtenerCursor()
+
+sql_crear_tablas = crear_tablas()
+
+if not sql_crear_tablas.strip():
+     print("revise la funcion crear tablas()")
+else:
+     cursor.execute(sql_crear_tablas)
+     connection.commit()
+
+cursor.close()
+connection.close()
+
